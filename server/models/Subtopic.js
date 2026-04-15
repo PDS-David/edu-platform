24c6df31ebc02b38bd327d260083d354e9a83407
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 18:22:07 +0100

    Update Subtopic.js

--- server/models/Subtopic.js
+++ server/models/Subtopic.js
@@ -1,45 +1,46 @@
+'use strict';
+
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const Subtopic = sequelize.define('Subtopic', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  return sequelize.define(
+    'Subtopic',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
 
-    topicId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'topic_id',
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      topic_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      subject_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      order_index: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
     },
-
-    subjectId: {
-      type: DataTypes.INTEGER,
-      field: 'subject_id',
-    },
-
-    name: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT },
-    content: { type: DataTypes.TEXT },
-
-    orderIndex: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      field: 'order_index',
-    },
-
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-      field: 'is_active',
-    },
-  }, {
-    tableName: 'subtopics',
-    underscored: true,
-    timestamps: true,
-  });
-
-  Subtopic.associate = (models) => {
-    Subtopic.belongsTo(models.Topic, { foreignKey: 'topic_id' });
-    Subtopic.belongsTo(models.Subject, { foreignKey: 'subject_id' });
-  };
-
-  return Subtopic;
+    {
+      tableName: 'subtopics',
+      timestamps: true,
+      underscored: true,
+    }
+  );
 };
