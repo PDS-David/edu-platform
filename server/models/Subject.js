9c561b534c26a89b280965e4654a9dd71a590bd8
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 18:18:03 +0100

    Update Subject.js

--- server/models/Subject.js
+++ server/models/Subject.js
@@ -1,27 +1,36 @@
+'use strict';
+
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const Subject = sequelize.define('Subject', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    examBoardId: { type: DataTypes.INTEGER, allowNull: false, field: 'exam_board_id' },
-    name: { type: DataTypes.STRING, allowNull: false },
-    code: { type: DataTypes.STRING },
-    level: { type: DataTypes.STRING }, // e.g. GCSE, A-Level
-    description: { type: DataTypes.TEXT },
-    imageUrl: { type: DataTypes.STRING, field: 'image_url' },
-    isActive: { type: DataTypes.BOOLEAN, defaultValue: true, field: 'is_active' },
-  }, {
-    tableName: 'subjects',
-    underscored: true,
-    timestamps: true,
-  });
+  return sequelize.define(
+    'Subject',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
 
-  Subject.associate = (models) => {
-    Subject.belongsTo(models.ExamBoard, { foreignKey: 'exam_board_id' });
-    Subject.hasMany(models.Topic, { foreignKey: 'subject_id' });
-    Subject.hasMany(models.Course, { foreignKey: 'subject_id' });
-    Subject.hasMany(models.PastPaper, { foreignKey: 'subject_id' });
-  };
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-  return Subject;
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      order_index: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+    },
+    {
+      tableName: 'subjects',
+      timestamps: true,
+      underscored: true,
+    }
+  );
 };
