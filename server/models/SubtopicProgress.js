e285002fdaca40dcc1f2b532d7c4d812d889cee0
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 18:17:19 +0100

    Update SubtopicProgress.js

--- server/models/SubtopicProgress.js
+++ server/models/SubtopicProgress.js
@@ -1,24 +1,61 @@
+'use strict';
+
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const SubtopicProgress = sequelize.define('SubtopicProgress', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    studentId: { type: DataTypes.UUID, allowNull: false, field: 'student_id' },
-    subtopicId: { type: DataTypes.INTEGER, allowNull: false, field: 'subtopic_id' },
-    resourcesCompleted: { type: DataTypes.BOOLEAN, defaultValue: false, field: 'resources_completed' },
-    practiceCompleted: { type: DataTypes.BOOLEAN, defaultValue: false, field: 'practice_completed' },
-    quizCompleted: { type: DataTypes.BOOLEAN, defaultValue: false, field: 'quiz_completed' },
-    updatedAt: { type: DataTypes.DATE, field: 'updated_at' },
-  }, {
-    tableName: 'subtopic_progress',
-    underscored: true,
-    timestamps: true,
-  });
+  const SubtopicProgress = sequelize.define(
+    'SubtopicProgress',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
 
-  SubtopicProgress.associate = (models) => {
-    SubtopicProgress.belongsTo(models.User, { foreignKey: 'student_id', as: 'student' });
-    SubtopicProgress.belongsTo(models.Subtopic, { foreignKey: 'subtopic_id' });
-  };
+      student_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+
+      subtopic_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      resources_completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+
+      practice_completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+
+      quiz_completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {
+      tableName: 'subtopic_progress',
+      timestamps: true,
+      underscored: true,
+
+      indexes: [
+        {
+          unique: true,
+          fields: ['student_id', 'subtopic_id'],
+        },
+        {
+          fields: ['student_id'],
+        },
+        {
+          fields: ['subtopic_id'],
+        },
+      ],
+    }
+  );
 
   return SubtopicProgress;
 };
