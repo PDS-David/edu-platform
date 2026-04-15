6323e00fa426e9549d2e405da7f1909d671c5a2c
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 19:50:11 +0100

    Update redis.js

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,1 +1,27 @@
+'use strict';
 
+const { createClient } = require('redis');
+const logger = require('./logger');
+
+const redis = createClient({
+  url: process.env.REDIS_URL
+});
+
+redis.on('error', (err) => {
+  logger.error('Redis error', { error: err.message });
+});
+
+redis.on('connect', () => {
+  logger.info('Redis connected');
+});
+
+async function connectRedis() {
+  if (!redis.isOpen) {
+    await redis.connect();
+  }
+}
+
+module.exports = {
+  redis,
+  connectRedis
+};
