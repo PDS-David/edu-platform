9087a36e0203bdd09906a2de493375927c71d3fe
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 16:57:28 +0100

    Update Topic.js

--- server/models/Topic.js
+++ server/models/Topic.js
@@ -3,11 +3,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   const Topic = sequelize.define('Topic', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    subjectId: { type: DataTypes.INTEGER, allowNull: false, field: 'subject_id' },
+
+    subjectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'subject_id',
+    },
+
     name: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.TEXT },
-    orderIndex: { type: DataTypes.INTEGER, defaultValue: 0, field: 'order_index' },
-    isActive: { type: DataTypes.BOOLEAN, defaultValue: true, field: 'is_active' },
+
+    orderIndex: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      field: 'order_index',
+    },
+
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      field: 'is_active',
+    },
   }, {
     tableName: 'topics',
     underscored: true,
@@ -17,8 +33,6 @@ module.exports = (sequelize) => {
   Topic.associate = (models) => {
     Topic.belongsTo(models.Subject, { foreignKey: 'subject_id' });
     Topic.hasMany(models.Subtopic, { foreignKey: 'topic_id' });
-    Topic.hasMany(models.Resource, { foreignKey: 'topic_id' });
-    Topic.hasMany(models.Quiz, { foreignKey: 'topic_id' });
   };
 
   return Topic;
