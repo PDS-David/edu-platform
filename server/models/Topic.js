79fe0d7b96d80a63c36630812dca2d52213b8e4d
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 18:18:52 +0100

    Update Topic.js

--- server/models/Topic.js
+++ server/models/Topic.js
@@ -1,39 +1,46 @@
+'use strict';
+
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const Topic = sequelize.define('Topic', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  return sequelize.define(
+    'Topic',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
 
-    subjectId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'subject_id',
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      subject_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      order_index: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
     },
-
-    name: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT },
-
-    orderIndex: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      field: 'order_index',
-    },
-
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-      field: 'is_active',
-    },
-  }, {
-    tableName: 'topics',
-    underscored: true,
-    timestamps: true,
-  });
-
-  Topic.associate = (models) => {
-    Topic.belongsTo(models.Subject, { foreignKey: 'subject_id' });
-    Topic.hasMany(models.Subtopic, { foreignKey: 'topic_id' });
-  };
-
-  return Topic;
+    {
+      tableName: 'topics',
+      timestamps: true,
+      underscored: true,
+    }
+  );
 };
