9f854d3f4f449c7872a2be6e7ad6b70b639b5609
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-15 16:58:16 +0100

    Update Subtopic.js

--- server/models/Subtopic.js
+++ server/models/Subtopic.js
@@ -3,13 +3,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   const Subtopic = sequelize.define('Subtopic', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    topicId: { type: DataTypes.INTEGER, allowNull: false, field: 'topic_id' },
-    subjectId: { type: DataTypes.INTEGER, field: 'subject_id' }, // used by userMemory raw SQL
+
+    topicId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'topic_id',
+    },
+
+    subjectId: {
+      type: DataTypes.INTEGER,
+      field: 'subject_id',
+    },
+
     name: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.TEXT },
     content: { type: DataTypes.TEXT },
-    orderIndex: { type: DataTypes.INTEGER, defaultValue: 0, field: 'order_index' },
-    isActive: { type: DataTypes.BOOLEAN, defaultValue: true, field: 'is_active' },
+
+    orderIndex: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      field: 'order_index',
+    },
+
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      field: 'is_active',
+    },
   }, {
     tableName: 'subtopics',
     underscored: true,
