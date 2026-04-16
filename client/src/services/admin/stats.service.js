b75cac26290fcaaebd75eefe80e7acd9b81960af
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 11:13:06 +0100

    Update stats.service.js

--- client/src/services/admin/stats.service.js
+++ client/src/services/admin/stats.service.js
@@ -1,1 +1,9 @@
+import { adminClient } from './adminClient';
 
+export const statsService = {
+  platformStats: () =>
+    adminClient.get('/admin/platform-stats'),
+
+  pendingCount: () =>
+    adminClient.get('/admin/questions/pending-count')
+};
