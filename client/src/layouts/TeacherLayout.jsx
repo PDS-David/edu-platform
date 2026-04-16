af9efa11e33a3a117fa43d0c3b92ab072a054e85
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 06:40:38 +0100

    Update TeacherLayout.jsx

--- client/src/layouts/TeacherLayout.jsx
+++ client/src/layouts/TeacherLayout.jsx
@@ -4,9 +4,7 @@ export default function TeacherLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <TopNav />
-      <div className="max-w-6xl mx-auto px-4 py-4">
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
