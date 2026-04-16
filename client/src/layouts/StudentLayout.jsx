3caf91a6950507064f7bcf4582cdb99e76482b4f
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 07:58:41 +0100

    Update StudentLayout.jsx

--- client/src/layouts/StudentLayout.jsx
+++ client/src/layouts/StudentLayout.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom";
 import TopNav from "../components/TopNav";
 
-export default function StudentLayout({ children }) {
+export default function StudentLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
       <TopNav />
       <div className="max-w-5xl mx-auto p-4">
-        {children}
+        <Outlet />
       </div>
     </div>
   );
