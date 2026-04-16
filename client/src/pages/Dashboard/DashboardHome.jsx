6883c5168b750d854abae0cb94460d22c8b5181f
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 09:04:48 +0100

    Update DashboardHome.jsx

--- client/src/pages/Dashboard/DashboardHome.jsx
+++ client/src/pages/Dashboard/DashboardHome.jsx
@@ -3,9 +3,9 @@ import useDashboardData from "../../hooks/useDashboardData";
 import useRealtimeSync from "../../hooks/useRealtimeSync";
 
 import ProgressSummary from "../../components/ProgressSummary";
-import WeakTopicsPanel from "../../components/dashboard/WeakTopicsPanel";
-import RecommendationPanel from "../../components/dashboard/RecommendationPanel";
-import SessionPanel from "../../components/dashboard/SessionPanel";
+import WeakTopicsPanel from "../../components/WeakTopicsPanel";
+import RecommendationPanel from "../../components/RecommendationPanel";
+import SessionPanel from "../../components/SessionPanel";
 
 export default function DashboardHome() {
   useRealtimeSync(); // 🔥 ONLY ADDITION (UI untouched)
