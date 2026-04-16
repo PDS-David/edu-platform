e3df2a0d9687929cb162757e192ed1f139db048d
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 07:59:36 +0100

    Update AdminLayout.jsx

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom";
 import TopNav from "../components/TopNav";
 
-export default function AdminLayout({ children }) {
+export default function AdminLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
       <TopNav />
-      <div className="max-w-6xl mx-auto px-4 py-4">
-        {children}
+      <div className="max-w-5xl mx-auto p-4">
+        <Outlet />
       </div>
     </div>
   );
