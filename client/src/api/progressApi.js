0ec3b7c896d7d591e350378d1aed700a1d1ffc08
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 02:21:08 +0100

    Update progressApi.js

--- client/src/api/progressApi.js
+++ client/src/api/progressApi.js
@@ -1,1 +1,7 @@
+import api from './axios';
 
+export const getSubtopics = (params) =>
+  api.get('/subtopics', { params });
+
+export const updateProgress = (data) =>
+  api.post('/progress/update', data);
