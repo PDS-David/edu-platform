8a09667db0871dc72bebd35f1b28f2601cc6fa6e
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 01:31:50 +0100

    Update eventBus.js

--- server/services/eventBus.js
+++ server/services/eventBus.js
@@ -1,1 +1,19 @@
+'use strict';
 
+const EventEmitter = require('events');
+
+/**
+ * Global application event bus
+ * Used for decoupling engines (progress, analytics, AI, etc.)
+ */
+class EventBus extends EventEmitter {
+  emitEvent(eventName, payload) {
+    this.emit(eventName, payload);
+  }
+
+  registerListener(eventName, handler) {
+    this.on(eventName, handler);
+  }
+}
+
+module.exports = new EventBus();
