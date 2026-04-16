5f2d13b8df6834b2ecb0634dd46fcb7687fc1dc7
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 06:39:05 +0100

    Update StudentLayout.jsx

--- client/src/layouts/StudentLayout.jsx
+++ client/src/layouts/StudentLayout.jsx
@@ -4,7 +4,7 @@ export default function StudentLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <TopNav />
-      <div className="max-w-6xl mx-auto px-4 py-4">
+      <div className="max-w-5xl mx-auto p-4">
         {children}
       </div>
     </div>
