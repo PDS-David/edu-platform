f116680ce7ec740c80fe0cf00b641aa9f6b32fd1
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 05:15:42 +0100

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
-import ProgressSummary from "../../components/dashboard/ProgressSummary";
-import WeakTopicsPanel from "../../components/dashboard/WeakTopicsPanel";
-import RecommendationPanel from "../../components/dashboard/RecommendationPanel";
-import SessionPanel from "../../components/dashboard/SessionPanel";
+import ProgressSummary from "../../components/ProgressSummary";
+import WeakTopicsPanel from "../../components/WeakTopicsPanel";
+import RecommendationPanel from "../../components/RecommendationPanel";
+import SessionPanel from "../../components/SessionPanel";
 
 export default function Dashboard() {
   return (
