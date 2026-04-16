779b815493f18c7d82db57e8fa7b3851371e49bd
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 02:35:57 +0100

    Create authApi.js

---
+++ client/src/api/authApi.js
@@ -0,0 +1,29 @@
+import api from './axios';
+
+/**
+ * LOGIN
+ */
+export const login = async (email, password) => {
+  const res = await api.post('/auth/login', {
+    email,
+    password,
+  });
+
+  return res.data;
+};
+
+/**
+ * REGISTER
+ */
+export const register = async (payload) => {
+  const res = await api.post('/auth/register', payload);
+  return res.data;
+};
+
+/**
+ * GET CURRENT USER
+ */
+export const getMe = async () => {
+  const res = await api.get('/users/me');
+  return res.data;
+};
