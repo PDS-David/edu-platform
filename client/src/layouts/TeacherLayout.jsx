a8f9a865b05504c5eed3996e452556393f167c4b
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-16 08:00:29 +0100

    Update TeacherLayout.jsx

--- client/src/layouts/TeacherLayout.jsx
+++ client/src/layouts/TeacherLayout.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router-dom";
 import TopNav from "../components/TopNav";
 
-export default function TeacherLayout({ children }) {
+export default function TeacherLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
       <TopNav />
-      {children}
+      <div className="max-w-5xl mx-auto p-4">
+        <Outlet />
+      </div>
     </div>
   );
 }
