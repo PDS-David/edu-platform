cea3bd720d1556d6b791ecd76bf07a1987aefde4
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-17 16:39:24 +0100

    Update authApi.js

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,22 +1,16 @@
-// client/src/api/authApi.js
-
-import api from '../services/api'; // ONLY SOURCE
+import api from '../services/api';
 
 export const login = async (email, password) => {
-  const res = await api.post('/auth/login', {
-    email,
-    password,
-  });
-
-  return res;
+  const res = await api.post('/auth/login', { email, password });
+  return res.data;
 };
 
 export const register = async (payload) => {
   const res = await api.post('/auth/register', payload);
-  return res;
+  return res.data;
 };
 
 export const getMe = async () => {
-  const res = await api.get('/users/me');
-  return res;
+  const res = await api.get('/auth/me');
+  return res.data;
 };
