ad21eb1d18a19dfb635091f91f55b928f0220e12
SowtheSeedIb <ssowtheseed@gmail.com>
2026-04-17 09:21:34 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,10 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/queryClient';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-root.render(
-  <QueryClientProvider client={queryClient}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </QueryClientProvider>
+  </React.StrictMode>
 );
